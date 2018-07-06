4c75cb69a2568e546270cebbf7bc6091464f642e
huasheng28 <huasheng28@qq.com>
2018-07-06 17:43:07 +0800

    add web folder

--- upload/cleancycle.go
+++ upload/cleancycle.go
@@ -6,6 +6,7 @@ import (
 	"upload"
 	"os"
 	"strconv"
+	"regexp"
 )
 
 func getImgNames(imgFolder string) []string {
@@ -19,41 +20,47 @@ func getImgNames(imgFolder string) []string {
 
 
 func cleanCycle(csvName string) {
-	upload.WriteTimeToFile()
+	url := "http://192.168.1.186:8080/v1/identify"
 	//url := upload.ApiUrlText(csvName)
-	//_, _, _, paramName, extraParam, headers := upload.ReadCsvFile(csvName)
+	_, _, _, paramName, extraParam, headers := upload.ReadCsvFile(csvName)
 	//把图片名称转换为图片地址
 	currentPath, _ := os.Getwd()
-	fmt.Println(currentPath)
+	//fmt.Println(currentPath)
 	imgNames := getImgNames(currentPath+"/image")
-	fmt.Println(imgNames)
+	//fmt.Println(imgNames)
 	var imgPath []string
 	for _, k := range imgNames {
-		absPath := currentPath + "/image/" + csvName + "/" + k
+		absPath := currentPath + "/image/" + k
 		imgPath = append(imgPath, absPath)
-		fmt.Println(imgPath)
 	}
 	//开始上传并输出
 	var respBody string
-	var total, timeout = 0, 0
 	for i, j := range imgPath {
 		fmt.Printf("正在测试[" + strconv.Itoa(i+1) + "/" + strconv.Itoa(len(imgNames)) + "]...")
 		respBody = upload.DoUpload(url, j, paramName, extraParam, headers)
-		if respBody == "" {
-			timeout++
-			fmt.Println("连接超时")
-			touText := imgNames[i] + "连接超时"
-			upload.WriteFile(touText)
-			continue
+		if respBody == "timeout" {
+			fmt.Println("%v 连接超时重试",imgNames[i])
+			aText := imgNames[i] + "连接超时重试"
+			upload.WriteFile(aText)
+			respBody = upload.DoUpload(url, j, paramName, extraParam, headers)
+		}else if ok, err :=regexp.MatchString("\"errcode\":50001",respBody);ok{
+			if err !=nil {
+				panic(err)
+			}
+			respBody = upload.DoUpload(url, j, paramName, extraParam, headers)
+			bText := imgNames[i] + "返回错误重试"
+			upload.WriteFile(bText)
 		}
-		total++
+		upload.WriteTimeToFile()
+		upload.WriteFile(j)
+		upload.WriteFile(respBody)
 	}
 	totalText := "测试完成"
 	fmt.Println(totalText)
+	upload.WriteTimeToFile()
 	upload.WriteFile(totalText)
 }
 
 func main() {
-	cleanCycle("https@api-intl%seeunsee%cn+v1+identify")
-
+	cleanCycle("http@192%168%1%1868080+v1+identify")
 }
--- web/main.go
+++ web/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strings"
 	"log"
+	"html/template"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -16,12 +17,25 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	for k, v := range r.Form {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
-	}
+	}	
 	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("请求方法：",r.Method)
+	if r.Method == "GET"{
+		t, _ := template.ParseFiles("login.gtpl")
+		log.Println(t.Execute(w,nil))
+	}else {
+		r.ParseForm()
+		fmt.Println("username",r.Form["username"])
+		fmt.Println("password",r.Form["password"])
+	}
 }
 
 func main() {
 	http.HandleFunc("/", sayhelloName) //设置访问的路由
+	http.HandleFunc("/login",login)
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
